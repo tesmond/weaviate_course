2fa23939a07a72c9e96ba70d609bb851063d3792
Andrew Gill <tesmond@gmail.com>
2025-12-15 11:18:51 +0000

    running

--- weaviate_course/course_1.py
+++ weaviate_course/course_1.py
@@ -1,9 +1,16 @@
+import json
+
 import weaviate
 
 headers = {
     # "X-Cohere-Api-Key": os.getenv("COHERE_APIKEY")
 }  # Replace with your Cohere API key
+try:
+    client = weaviate.connect_to_local(headers=headers)
+    print("Weaviate is ready:", client.is_ready())
 
-client = weaviate.connect_to_local(headers=headers)
-print("Weaviate is ready:", client.is_ready())
-client.close()
+    assert client.is_ready()
+    metainfo = client.get_meta()
+    print(json.dumps(metainfo, indent=2))
+finally:
+    client.close()
