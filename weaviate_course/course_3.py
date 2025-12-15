e95bce9a023acb169ab41a66a1233f891ca19322
Andrew Gill <tesmond@gmail.com>
2025-12-15 11:37:22 +0000

    create collection

---
+++ weaviate_course/course_3.py
@@ -0,0 +1,55 @@
+import json
+import os
+from datetime import datetime, timezone
+
+import pandas as pd
+import requests
+import weaviate
+from tqdm import tqdm
+from weaviate.util import generate_uuid5
+
+headers = {
+    "X-Cohere-Api-Key": os.getenv("COHERE_APIKEY")
+}  # Replace with your Cohere API key
+
+client = weaviate.connect_to_local(headers=headers)
+
+
+data_url = "https://raw.githubusercontent.com/weaviate-tutorials/edu-datasets/main/movies_data_1990_2024.json"
+resp = requests.get(data_url)
+df = pd.DataFrame(resp.json())
+
+# Configure collection object
+movies = client.collections.use("Movies")
+
+# Enter context manager
+with movies.batch.fixed_size(batch_size=200) as batch:
+    # Loop through the data
+    for i, movie in tqdm(df.iterrows()):
+        # Convert data types
+        # Convert a JSON date to `datetime` and add time zone information
+        release_date = datetime.fromisoformat(movie["release_date"]).replace(
+            tzinfo=timezone.utc
+        )
+        # Convert a JSON array to a list of integers
+        genre_ids = json.loads(movie["genre_ids"])
+
+        # Build the object payload
+        movie_obj = {
+            "title": movie["title"],
+            "overview": movie["overview"],
+            "vote_average": movie["vote_average"],
+            "genre_ids": genre_ids,
+            "release_date": release_date,
+            "tmdb_id": movie["id"],
+        }
+
+        # Add object to batch queue
+        batch.add_object(properties=movie_obj, uuid=generate_uuid5(movie["id"]))
+        # Batcher automatically sends batches
+
+# Check for failed objects
+if len(movies.batch.failed_objects) > 0:
+    print(f"Failed to import {len(movies.batch.failed_objects)} objects")
+
+client.close()
